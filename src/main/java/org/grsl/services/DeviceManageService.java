fb44f23b1718ac6d07616169b87dffc0279086ec
r1cardohj <2413302357@qq.com>
2024-07-09 01:33:09 +0800

    Modify updateDevice api action

--- src/main/java/org/grsl/services/DeviceManageService.java
+++ src/main/java/org/grsl/services/DeviceManageService.java
@@ -6,6 +6,7 @@ import org.grsl.utils.Pager;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DeviceManageService {
@@ -20,8 +21,8 @@ public class DeviceManageService {
     }
 
     public List<Device> findDeviceByPage(Integer page, Integer perPage) {
-        Pager pagger = new Pager(page, perPage);
-        return this.deviceRespository.findAllDeviceByPage(pagger.getLimit(), pagger.getOffset());
+        Pager pager = new Pager(page, perPage);
+        return this.deviceRespository.findAllDeviceByPage(pager.getLimit(), pager.getOffset());
     }
 
 
@@ -32,14 +33,32 @@ public class DeviceManageService {
     }
 
     public void updateDevice(Device device) {
-        if (!this.deviceRespository.existsById(device.getId()))
-            throw new DeviceRepository.DeviceNotFoundException();
-        this.deviceRespository.save(device);
+        Device baseDevice = this.findDeviceById(device.getId());
+        Device mergedDevice = this.getMergedDevice(baseDevice, device);
+        if (mergedDevice.equals(baseDevice))
+            return;
+        this.deviceRespository.save(mergedDevice);
     }
 
     public void deleteDeviceById(long id) {
         if (!this.deviceRespository.existsById(id))
             throw new DeviceRepository.DeviceNotFoundException();
         this.deviceRespository.deleteById(id);
+    }
+
+    public Device getMergedDevice(Device d1, Device d2) {
+        Device mergedDevice = new Device();
+        mergedDevice.setId(d2.getId());
+        mergedDevice.setName(Optional.ofNullable(d2.getName()).orElse(d1.getName()));
+        mergedDevice.setDeviceCode(Optional.ofNullable(d2.getDeviceCode()).orElse(d1.getDeviceCode()));
+        mergedDevice.setSpecifications(Optional.ofNullable(d2.getSpecifications()).orElse(d1.getSpecifications()));
+        mergedDevice.setPosition(Optional.ofNullable(d2.getPosition()).orElse(d1.getPosition()));
+        mergedDevice.setLightingType(Optional.ofNullable(d2.getLightingType()).orElse(d1.getLightingType()));
+        mergedDevice.setIsOnline(Optional.ofNullable(d2.getIsOnline()).orElse(d1.getIsOnline()));
+        mergedDevice.setSerialNumber(Optional.ofNullable(d2.getSerialNumber()).orElse(d1.getSerialNumber()));
+        mergedDevice.setChargeBy(Optional.ofNullable(d2.getChargeBy()).orElse(d1.getChargeBy()));
+        mergedDevice.setExFactoryDate(Optional.ofNullable(d2.getExFactoryDate()).orElse(d1.getExFactoryDate()));
+        mergedDevice.setExpiredDate(Optional.ofNullable(d2.getExpiredDate()).orElse(d1.getExpiredDate()));
+        return mergedDevice;
     }
 }

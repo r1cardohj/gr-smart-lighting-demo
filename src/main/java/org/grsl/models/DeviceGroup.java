ad02302f2f1b07c1323968aee58082add37a0731
r1cardohj <2413302357@qq.com>
2024-07-09 02:22:12 +0800

    Update DeviceGroup model property.

--- src/main/java/org/grsl/models/DeviceGroup.java
+++ src/main/java/org/grsl/models/DeviceGroup.java
@@ -3,12 +3,11 @@ package org.grsl.models;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
-import java.util.List;
 
 @Data
 public class DeviceGroup implements BaseModel{
     @Id
     private long id;
     private String name;
-    private List<Device> devices;
+    private String description;
 }
